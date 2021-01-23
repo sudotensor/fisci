ffe7ad1c4ec4bcbe706739143eeece6e4f10d2cf
Brian Evans <ebrian101@gmail.com>
2021-01-23 14:42:35 +0000

    Added database connection
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- api/api.py
+++ api/api.py
@@ -1,8 +1,11 @@
 import time
 from flask import Flask
+import connect_database
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    session = connect_database.get_database_client()
+    t = session.execute("SELECT id FROM fisci.test_table WHERE id>314160 ALLOW FILTERING;").one()[0]
+    return {'time': t}
---
+++ api/connect_database.py
@@ -0,0 +1,21 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+
+def get_database_client():
+    """
+    To query the database:
+    ```
+    session = get_database_client()
+    result = session.execute("SQL")
+    ```
+    :return: cassandra database client
+    """
+    cloud_config = {
+            'secure_connect_bundle': 'secure-connect-database.zip'
+    }
+    auth_provider = PlainTextAuthProvider('fisci', '46xcNrETE@csg9c')
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session = cluster.connect()
+    return session
+
