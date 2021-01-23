5d5a078ea8560834381fda79045b324424d4d794
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-01-23 14:27:36 +0000

    Setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyAZhqDGdEcLR4fc4uIhpPhavW_dRnSsV0E",
+    authDomain: "fisci-app-302613.firebaseapp.com",
+    projectId: "fisci-app-302613",
+    storageBucket: "fisci-app-302613.appspot.com",
+    messagingSenderId: "997735762224",
+    appId: "1:997735762224:web:b402869e132f01ce6520f5"
+};
+
+firebase.initializeApp(config);
+export default firebase;
