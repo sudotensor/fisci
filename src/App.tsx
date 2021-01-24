915135496699030354ec37b5185120215d50982d
Brian Evans <ebrian101@gmail.com>
2021-01-24 13:52:27 +0000

    Added firebase auth context
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,18 @@ import { OverviewPage } from "./pages/OverviewPage";
 import { InsightsPage } from "./pages/InsightsPage";
 import { TransactionsPage } from "./pages/TransactionsPage";
 import { GuidesPage } from "./pages/GuidesPage";
+import { LoginContext } from "./LoginContext";
+import firebase from "firebase/app";
+import "firebase/auth";
+import fire from "./firebase";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  fire.auth().onAuthStateChanged((u: firebase.User | null) => {
+    u ? setIsLoggedIn(true) : setIsLoggedIn(false);
+    console.log("Updated isLoggedIn", isLoggedIn);
+  });
 
   useEffect(() => {
     fetch("https://fisci-iqk43m4jga-ew.a.run.app/time")
@@ -21,46 +30,48 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Header />
-        </header>
-        <nav>
-          <h2>Fisci</h2>
-          <div>
-            <Link to={"/"}>Overview</Link>
-            <Link to={"/transactions"}>Transactions</Link>
-            <Link to={"/insights"}>Insights</Link>
-            <Link to={"/guides"}>Guides for understanding finance</Link>
-          </div>
-          <Link to={"/login"}>Login</Link>
-        </nav>
-        <main>
-          <Switch>
-            <Route path={"/login"}>
-              <LoginPage />
-            </Route>
-            <Route path={"/transactions"}>
-              <TransactionsPage />
-            </Route>
-            <Route path={"/insights"}>
-              <InsightsPage />
-            </Route>
-            <Route path={"/guides"}>
-              <GuidesPage />
-            </Route>
-            <Route path={"/"}>
-              <OverviewPage />
-            </Route>
-          </Switch>
-          <p id={"api-test"}>
-            <code>API TEST: </code>The current time is{" "}
-            {new Date(currentTime * 1000).toLocaleTimeString()}
-          </p>
-        </main>
-      </div>
-    </Router>
+    <LoginContext.Provider value={isLoggedIn}>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+          </header>
+          <nav>
+            <h2>Fisci</h2>
+            <div>
+              <Link to={"/"}>Overview</Link>
+              <Link to={"/transactions"}>Transactions</Link>
+              <Link to={"/insights"}>Insights</Link>
+              <Link to={"/guides"}>Guides for understanding finance</Link>
+            </div>
+            <Link to={"/login"}>Login</Link>
+          </nav>
+          <main>
+            <Switch>
+              <Route path={"/login"}>
+                <LoginPage />
+              </Route>
+              <Route path={"/transactions"}>
+                <TransactionsPage />
+              </Route>
+              <Route path={"/insights"}>
+                <InsightsPage />
+              </Route>
+              <Route path={"/guides"}>
+                <GuidesPage />
+              </Route>
+              <Route path={"/"}>
+                <OverviewPage />
+              </Route>
+            </Switch>
+            <p id={"api-test"}>
+              <code>API TEST: </code>The current time is{" "}
+              {new Date(currentTime * 1000).toLocaleTimeString()}
+            </p>
+          </main>
+        </div>
+      </Router>
+    </LoginContext.Provider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,20 @@
-import firebase from 'firebase'
+import firebase from "firebase/app";
 
-const config = {
-    apiKey: "AIzaSyAZhqDGdEcLR4fc4uIhpPhavW_dRnSsV0E",
-    authDomain: "fisci-app-302613.firebaseapp.com",
-    projectId: "fisci-app-302613",
-    storageBucket: "fisci-app-302613.appspot.com",
-    messagingSenderId: "997735762224",
-    appId: "1:997735762224:web:b402869e132f01ce6520f5"
+// this is bad coding practice to have an API key in a public repo
+// if this were a real app we would store this somewhere else (.env)
+// we only put it here for convenience sake.
+export const config = {
+  apiKey: "AIzaSyAZhqDGdEcLR4fc4uIhpPhavW_dRnSsV0E",
+  authDomain: "fisci-app-302613.firebaseapp.com",
+  projectId: "fisci-app-302613",
+  storageBucket: "fisci-app-302613.appspot.com",
+  messagingSenderId: "997735762224",
+  appId: "1:997735762224:web:b402869e132f01ce6520f5",
 };
 
-firebase.initializeApp(config);
+if (firebase.apps.length === 0) firebase.initializeApp(config);
+
+console.log("is logged in? ", firebase.apps.length);
+
+const fire = firebase;
 export default firebase;
