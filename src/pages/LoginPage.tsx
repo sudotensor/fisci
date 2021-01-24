0ea9bfe58c06f25d02a8803ed2ca04c2ddae4573
Brian Evans <ebrian101@gmail.com>
2021-01-24 16:55:14 +0000

    Insights page
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import "./login.css";
-import { LoginContext } from "../LoginContext";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,7 +8,6 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
@@ -53,7 +51,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function LoginPage(props: Props) {
-  const loggedIn: boolean = React.useContext(LoginContext);
   const classes = useStyles();
 
   return (
