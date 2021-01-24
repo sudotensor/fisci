a704c5eafef3db2ea1c1d43323daceff589c86db
bryangwj <bryangwj@gmail.com>
2021-01-24 06:57:40 +0000

    Add upload_transaction to app.py

--- api/app.py
+++ api/app.py
@@ -46,5 +46,28 @@ def upload_transaction_csv():
         return {"msg": "Nothing happened. Please POST the csv file"}
 
 
+@app.route('/trans/upload', methods = ["POST"])
+def upload_transaction():
+
+    """User send transaction details to be uploaded to the database
+    Data is received in json format"""
+
+    data = request.get_json() or {}
+    if 'date' not in data or 'shop_name' not in data or 'amount' not in data:
+        return bad_request('must include date, shop name and amount fields')
+    
+    session = get_database_client()
+
+    session.execute("INSERT INTO fisci.transactions (transaction_id, user_id, shop_name, category, labeled, amount, date)
+                    VALUES (data['transaction_id'], data['userid'], data['shop_name'], data['category'], data['labeled'], data['amount'], data['date']);")
+                    
+    
+    return {"msg": "success"}
+
+
+def bad_request(message):
+    return error_response(400, message)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
