49cd4b4f1b4ee0589982d8a774bc248a76683db0
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-01-24 14:31:18 +0000

    Update UI to use Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,122 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { OverviewPage } from "./pages/OverviewPage";
 import { InsightsPage } from "./pages/InsightsPage";
 import { TransactionsPage } from "./pages/TransactionsPage";
 import { GuidesPage } from "./pages/GuidesPage";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import MenuIcon from "@material-ui/icons/Menu";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import List from "@material-ui/core/List";
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { makeStyles } from "@material-ui/core/styles";
+import { mainListItems, secondaryListItems } from "./listItems";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import clsx from "clsx";
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    backgroundColor: "#2B3036",
+    color: "white",
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    backgroundColor: "#1F2428",
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: "none",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     fetch("https://fisci-iqk43m4jga-ew.a.run.app/time")
@@ -22,42 +129,83 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <Header />
-        </header>
-        <nav>
-          <h2>Fisci</h2>
-          <div>
-            <Link to={"/"}>Overview</Link>
-            <Link to={"/transactions"}>Transactions</Link>
-            <Link to={"/insights"}>Insights</Link>
-            <Link to={"/guides"}>Guides for understanding finance</Link>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={clsx(classes.appBar, open && classes.appBarShift)}
+        >
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(
+                classes.menuButton,
+                open && classes.menuButtonHidden
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
+              Fisci
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
           </div>
-          <Link to={"/login"}>Login</Link>
-        </nav>
-        <main>
-          <Switch>
-            <Route path={"/login"}>
-              <LoginPage />
-            </Route>
-            <Route path={"/transactions"}>
-              <TransactionsPage />
-            </Route>
-            <Route path={"/insights"}>
-              <InsightsPage />
-            </Route>
-            <Route path={"/guides"}>
-              <GuidesPage />
-            </Route>
-            <Route path={"/"}>
-              <OverviewPage />
-            </Route>
-          </Switch>
-          <p id={"api-test"}>
-            <code>API TEST: </code>The current time is{" "}
-            {new Date(currentTime * 1000).toLocaleTimeString()}
-          </p>
+          <Divider />
+          <List>{mainListItems}</List>
+          <Divider />
+          <List>{secondaryListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Switch>
+              <Route path={"/login"}>
+                <LoginPage />
+              </Route>
+              <Route path={"/transactions"}>
+                <TransactionsPage />
+              </Route>
+              <Route path={"/insights"}>
+                <InsightsPage />
+              </Route>
+              <Route path={"/guides"}>
+                <GuidesPage />
+              </Route>
+              <Route path={"/"}>
+                <OverviewPage />
+              </Route>
+            </Switch>
+            <p id={"api-test"}>
+              <code>API TEST: </code>The current time is{" "}
+              {new Date(currentTime * 1000).toLocaleTimeString()}
+            </p>
+          </Container>
         </main>
       </div>
     </Router>
---
+++ src/listItems.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import BankIcon from "@material-ui/icons/AccountBalance";
+import LibraryIcon from "@material-ui/icons/LibraryBooks";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import { Link } from "react-router-dom";
+
+export const mainListItems = (
+  <div>
+    <ListItem button component={Link} to={"/"}>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Overview" />
+    </ListItem>
+    <ListItem button component={Link} to={"/transactions"}>
+      <ListItemIcon>
+        <BankIcon />
+      </ListItemIcon>
+      <ListItemText primary="Transactions" />
+    </ListItem>
+    <ListItem button component={Link} to={"/insights"}>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Insights" />
+    </ListItem>
+    <ListItem button component={Link} to={"/guides"}>
+      <ListItemIcon>
+        <LibraryIcon />
+      </ListItemIcon>
+      <ListItemText primary="Guides" />
+    </ListItem>
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListSubheader inset>Saved reports</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItem>
+  </div>
+);
